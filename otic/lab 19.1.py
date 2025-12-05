7e23b5e50be608eeb6cd648106ed42a863012da3
eve <lina.ltvnk.2008@gmail.com>
2025-12-05 21:05:23 +0200

    Update lab 19.1.py

--- otic/lab 19.1.py
+++ otic/lab 19.1.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+fname= 'masiv.csv'
+df= pd.read_csv('masiv.csv', header=None, names=['Name', 'Phone', 'City'])
+name= input("Name")
+result= df[df['Name'] == name]
+if result.empty:
+    print("Not found")
+else:
+    print("Information of user:")
+    print(result.to_string(index=False))
